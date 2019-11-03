1dd4822ca7ba39873c93fabe070b96ad1ddc9ee9
Jose Soto <jose@xoolix.com>
2019-11-03 10:30:30 -0300

    card sin boton

--- src/CardEdificio.js
+++ src/CardEdificio.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export default function CardEdificio({ id, nombre, direccion, className }) {
   return (
     <div className={className}>
       <h2>{nombre}</h2>
       <h4>Direccion: {direccion}</h4>
-      <button className="button">
-        <Link to={`detalleEdificio/${id}`}>Ver unidades</Link>
-      </button>
     </div>
   );
 }
--- src/DetalleEdificio.js
+++ src/DetalleEdificio.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Nav from './Nav';
 
 export default function DetalleEdificio({ id, nombre }) {
   return (
     <div>
       <h2>{nombre}</h2>
+      <Nav />
     </div>
   );
 }
