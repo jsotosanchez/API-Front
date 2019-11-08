f0a0df5fb9d7e4e19f1ac902fdfed3a38643223d
Jose Soto <jose@xoolix.com>
2019-11-08 09:33:10 -0300

    botton salir pop up

--- src/UnidadPopUp.js
+++ src/UnidadPopUp.js
@@ -4,6 +4,7 @@ export default function UnidadPopUp() {
   console.log('unidad popup');
   return (
     <div className="modal">
+      <button className="close">X</button>
       <div className="modal-content">
         <h1>POP UP</h1>
       </div>
