ff8fbe15c1ebec1046f5324bb642ddf806bf6296
Jose Soto <jose@xoolix.com>
2019-11-24 12:22:13 -0300

    reclamos view by persona

--- src/Reclamos.js
+++ src/Reclamos.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ListaReclamos from './ListaReclamos';
-
-const fetchReclamos = async () => {
-  const data = await fetch(`http://localhost:8080/reclamos`);
-  const dataAsJson = await data.json();
-  return dataAsJson;
-};
+import { useSessionContext } from './SessionContext';
+import { fetchToServer } from './http';
 
 export default function Reclamos({ match }) {
+  const context = useSessionContext();
+
+  const fetchReclamos = async () => {
+    const d = await fetchToServer('http://localhost:8080/reclamos', context);
+    return d;
+  };
+
   return (
     <div>
       <ListaReclamos match={match} fetchReclamos={fetchReclamos} labelClass="texto-blanco" />
