796957d0ad455c024bf7062dffbccbcde9d874ce
Jose Soto <jose@xoolix.com>
2019-11-24 22:53:33 -0300

    ver documento admin only

--- src/CardPersona.js
+++ src/CardPersona.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import AdminOnly from './AdminOnly';
 
 export default function CardPersona({ nombre, documento }) {
   return (
     <div>
       <div className="card-no-over">
         <h5>Nombre: {nombre}</h5>
-        <h5>Documento: {documento}</h5>
+        <AdminOnly>
+          <h5>Documento: {documento}</h5>
+        </AdminOnly>
       </div>
     </div>
   );
