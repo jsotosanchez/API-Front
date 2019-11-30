7d9f8b37e2c78d8633562245567edb19b456d7ee
Jose Soto <jose@xoolix.com>
2019-11-30 14:07:01 -0300

    disable warning

--- src/hooks/useRefresh.js
+++ src/hooks/useRefresh.js
@@ -2,6 +2,7 @@ import { useState, useMemo } from 'react';
 
 export function useRefresh() {
   const [refreshId, setRefresh] = useState(0);
+  // eslint-disable-next-line
   const refresh = useMemo(() => () => setRefresh(refreshId + 1), []);
 
   return { refreshId, refresh };
